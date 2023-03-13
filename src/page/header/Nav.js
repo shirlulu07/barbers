43dd01bcd5ff59dcb7cdfbe0f773aa2511bfc0c1
shirlulu007 <alainlado07@gmail.com>
2023-03-13 16:05:27 +0800

    add

--- src/page/header/Nav.js
+++ src/page/header/Nav.js
@@ -18,12 +18,12 @@ function Nav() {
           <path d="M0 0h24v24H0z" fill="none" />
           <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
         </svg>
-        <ul className="justify-between gap-10 hidden sm:flex">
+        <ul className="justify-between gap-10 hidden poppins sm:flex">
           <li>
-            <AnchorLink href="#home" className={textStyle}>HOME</AnchorLink>
+            <AnchorLink href="#home" className="hover ">HOME</AnchorLink>
           </li>
           <li>
-            <AnchorLink href="#aboutUs" className={textStyle}>About Us</AnchorLink>
+            <AnchorLink href="#aboutUs" className="hover">About Us</AnchorLink>
           </li>
           <li>
             <AnchorLink href="#services" className={textStyle}>Services</AnchorLink>
